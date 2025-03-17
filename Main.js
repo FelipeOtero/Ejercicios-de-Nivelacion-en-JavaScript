f73f0e18d729dc9a4df855b455a290c26e3cff82
FelipeOtero <48389979@est.ort.edu.ar>
2025-03-17 01:34:19 -0300

    Add files via upload

--- Main.js
+++ Main.js
@@ -17,11 +17,11 @@ let divisionTotal = num1 / num2;
 let multiplicacionTotal = num1 * num2;
 
 console.log(`
-            Suma es ${sumaTotal} 
-            Resta es ${restaTotal}
-            Division es ${divisionTotal} 
-            Multiplicacion es ${multiplicacionTotal}
-            `);
+    Suma es ${sumaTotal} 
+    Resta es ${restaTotal}
+    Division es ${divisionTotal} 
+    Multiplicacion es ${multiplicacionTotal}
+    `);
 }
 
 
@@ -39,8 +39,44 @@ function parOimpar()
         console.log("Es impar")
     }
   
-  
+
+}
+
+function contarCaracteres()
+{  let palabra;
+    do 
+    {   palabra = document.getElementById('palabra').value   
+
+        if(palabra === "")
+        {   
+            console.log("Ingrese, una palabra");       
+            return;
+        }
+
+    }
+     while(palabra === "");
+
+     let cantidadCaracteres = palabra.length;
+    console.log(`La Palabra ${palabra} tiene ${cantidadCaracteres} caracteres`);
+}
 
 
+function repetirFrase()
+{  let palabra=document.getElementById('frase').value
+    let numero=document.getElementById('numero').value    
 
+    for (let i = 0; i < numero; i++) 
+        {
+         console.log(palabra);
+        
+        }
+}   
+
+function tablaMultiplicar()
+{   let numero = document.getElementById("numeroMultiplicar").value
+    console.log("Tabla de multiplicar del numero " + numero);
+    for (let i = 1; i <=10; i++) 
+    { let numeroMultiplicado = i*numero;
+       console.log(`${i}*${numero}= ${numeroMultiplicado}`)        
+    }
 }
